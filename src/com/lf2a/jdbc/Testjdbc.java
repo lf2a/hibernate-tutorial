5608fe2e6140767650668eccf84ffc64da82461b
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-07-31 11:37:20 -0300

    first commit

---
+++ src/com/lf2a/jdbc/Testjdbc.java
@@ -0,0 +1,22 @@
+package com.lf2a.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Testjdbc {
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimezone=UTC";
+        String user = "root";
+        String pass = "root";
+
+        try {
+            System.out.println("Conectando ao banco de dados: " + jdbcURL);
+
+            Connection conn = DriverManager.getConnection(jdbcURL, user, pass);
+
+            System.out.println("Conectado!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
