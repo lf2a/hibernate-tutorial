2b5c853f5b33165de044a296483f729071aaef57
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-08-09 11:43:19 -0300

    OneToMany - Unidirectional

---
+++ src/com/lf2a/hibernate/demo/oneToManyUni/GetInstructorDetailDemo.java
@@ -0,0 +1,61 @@
+package com.lf2a.hibernate.demo.oneToManyUni;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.lf2a.hibernate.demo.oneToManyUni.entity.Instructor;
+import com.lf2a.hibernate.demo.oneToManyUni.entity.InstructorDetail;
+
+public class GetInstructorDetailDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the instructor detail object
+            int theId = 2999;
+            InstructorDetail tempInstructorDetail =
+                    session.get(InstructorDetail.class, theId);
+
+            // print the instructor detail
+            System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+
+            // print  the associated instructor
+            System.out.println("the associated instructor: " +
+                    tempInstructorDetail.getInstructor());
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        } finally {
+            // handle connection leak issue
+            session.close();
+
+            factory.close();
+        }
+    }
+
+}
+
+
+
+
+
