0fe06724f8a1051855676a81ee7fc2f808ba5e33
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-08-10 17:34:52 -0300

    many to many

---
+++ src/com/lf2a/hibernate/demo/manyToMany/DeleteMaryStudentDemo.java
@@ -0,0 +1,64 @@
+package com.lf2a.hibernate.demo.manyToMany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.lf2a.hibernate.demo.manyToMany.entity.Course;
+import com.lf2a.hibernate.demo.manyToMany.entity.Instructor;
+import com.lf2a.hibernate.demo.manyToMany.entity.InstructorDetail;
+import com.lf2a.hibernate.demo.manyToMany.entity.Review;
+import com.lf2a.hibernate.demo.manyToMany.entity.Student;
+
+public class DeleteMaryStudentDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the student from database
+            int studentId = 2;
+            Student tempStudent = session.get(Student.class, studentId);
+
+            System.out.println("\nLoaded student: " + tempStudent);
+            System.out.println("Courses: " + tempStudent.getCourses());
+
+            // delete student
+            System.out.println("\nDeleting student: " + tempStudent);
+            session.delete(tempStudent);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } finally {
+
+            // add clean up code
+            session.close();
+
+            factory.close();
+        }
+    }
+
+}
+
+
+
+
+
